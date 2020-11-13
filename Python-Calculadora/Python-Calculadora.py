a73eba31c2916532853bbac8621017bbe4e17e24
vinicius-feitoza <69653992+vinicius-feitoza@users.noreply.github.com>
2020-11-13 18:02:53 -0300

    Update Python-Calculadora.py
    Adicionado visor que mostra a operação realizada a acima do resultado.
    Números agora aparecem a direita.
    Janela não é mais redimensionável.

--- Python-Calculadora/Python-Calculadora.py
+++ Python-Calculadora/Python-Calculadora.py
@@ -2,10 +2,21 @@ from tkinter import *
 
 root = Tk()
 root.title("Calculadora")
+root.geometry("200x290")
+root.resizable(False, False)
 
-e = Entry(root, width=20, borderwidth=2, font=14)
-e.grid(row=0, columnspan=3)
+e = Entry(root, width=20, borderwidth=2, font=14, justify='right')
+e.grid(row=1, columnspan=3)
 e.get()
+
+space = Label(root, text=" ")
+space.grid(row=0, column=2, columnspan=3)
+
+limpo = "                                                                                                       "
+l = Label(root, text=limpo)
+l.place(relx=1,
+        rely=0.0,
+        anchor='ne')
 
 def numero1():
     final = e.get()
@@ -59,6 +70,8 @@ def numero0():
 
 def limpar():
     e.delete(0, 'end')
+    l = Label(root, text=limpo)
+    l.place(relx = 1, rely = 0.0, anchor ='ne')
 
 def somar():
     global operador
@@ -68,6 +81,14 @@ def somar():
     p_num = float(primeiro_numero)
     e.delete(0, 'end')
 
+    l = Label(root, text=limpo)
+    l.place(relx = 1, rely = 0.0, anchor ='ne')
+
+    preview = Label(root, fg="#696969", text=str(primeiro_numero)+" +")
+    preview.place(relx = 1.0, rely = 0.0, anchor ='ne')
+
+
+
 def dividir():
     global operador
     operador = "divisao"
@@ -75,6 +96,12 @@ def dividir():
     global p_num
     p_num = float(primeiro_numero)
     e.delete(0, 'end')
+
+    l = Label(root, text=limpo)
+    l.place(relx = 1, rely = 0.0, anchor ='ne')
+
+    preview = Label(root, fg="#696969", text=str(primeiro_numero)+" /")
+    preview.place(relx = 1.0, rely = 0.0, anchor ='ne')
 
 def subtrair():
     global operador
@@ -84,6 +111,12 @@ def subtrair():
     p_num = float(primeiro_numero)
     e.delete(0, 'end')
 
+    l = Label(root, text=limpo)
+    l.place(relx = 1, rely = 0.0, anchor ='ne')
+
+    preview = Label(root, fg="#696969", text=str(primeiro_numero)+" -")
+    preview.place(relx = 1.0, rely = 0.0, anchor ='ne')
+
 def multiplicar():
     global operador
     operador = "multiplicacao"
@@ -92,38 +125,54 @@ def multiplicar():
     p_num = float(primeiro_numero)
     e.delete(0, 'end')
 
+    l = Label(root, text=limpo)
+    l.place(relx = 1, rely = 0.0, anchor ='ne')
+
+    preview = Label(root, fg="#696969", text=str(primeiro_numero)+" *")
+    preview.place(relx = 1.0, rely = 0.0, anchor ='ne')
+
 def result():
     segundo_numero = float(e.get())
     e.delete(0, 'end')
 
     if operador == "soma":
+        op = "+"
         resposta = (p_num + segundo_numero)
     if operador == "multiplicacao":
+        op = "*"
         resposta = (p_num * segundo_numero)
     if operador == "subtracao":
+        op = "-"
         resposta = (p_num - segundo_numero)
     if operador == "divisao":
+        op = "/"
         resposta = (p_num / segundo_numero)
 
     e.insert(0, resposta)
 
-Num1 = Button(root, text="1", command=numero1, width=8, height=2).grid(row=1, column=0)
-Num2 = Button(root, text="2", command=numero2, width=8, height=2).grid(row=1, column=1)
-Num3 = Button(root, text="3", command=numero3, width=8, height=2).grid(row=1, column=2)
-Num4 = Button(root, text="4", command=numero4, width=8, height=2).grid(row=2, column=0)
-Num5 = Button(root, text="5", command=numero5, width=8, height=2).grid(row=2, column=1)
-Num6 = Button(root, text="6", command=numero6, width=8, height=2).grid(row=2, column=2)
-Num7 = Button(root, text="7", command=numero7, width=8, height=2).grid(row=3, column=0)
-Num8 = Button(root, text="8", command=numero8, width=8, height=2).grid(row=3, column=1)
-Num9 = Button(root, text="9", command=numero9, width=8, height=2).grid(row=3, column=2)
-Num0 = Button(root, text="0", command=numero0, width=8, height=2).grid(row=4, column=0)
 
-Somar = Button(root, text="+", command = somar, width=8, height=2).grid(row=5, column=0)
-Dividir = Button(root, text="/", command = dividir, width=8, height=2).grid(row=6, column=0)
-Multiplicar = Button(root, text="*", command = multiplicar, width=8, height=2).grid(row=6, column=1)
-Subtrair = Button(root, text="-", command = subtrair, width=8, height=2).grid(row=6, column=2)
+    l.place(relx = 1, rely = 0.0, anchor ='ne')
 
-Clear = Button(root, text="Limpar", command=limpar, width=18, height=2).grid(row=4, column=1, columnspan=2)
-Resultado = Button(root, text="=", command = result, width=18, height=2).grid(row=5, column=1, columnspan=2)
+    historico = Label(root, fg="#696969", text=str(p_num) + " " + str(op) + " " + str(segundo_numero) + " =")
+    historico.place(relx = 1.0, rely = 0.0, anchor ='ne')
+
+Num1 = Button(root, text="1", command=numero1, width=8, height=2).grid(row=2, column=0)
+Num2 = Button(root, text="2", command=numero2, width=8, height=2).grid(row=2, column=1)
+Num3 = Button(root, text="3", command=numero3, width=8, height=2).grid(row=2, column=2)
+Num4 = Button(root, text="4", command=numero4, width=8, height=2).grid(row=3, column=0)
+Num5 = Button(root, text="5", command=numero5, width=8, height=2).grid(row=3, column=1)
+Num6 = Button(root, text="6", command=numero6, width=8, height=2).grid(row=3, column=2)
+Num7 = Button(root, text="7", command=numero7, width=8, height=2).grid(row=4, column=0)
+Num8 = Button(root, text="8", command=numero8, width=8, height=2).grid(row=4, column=1)
+Num9 = Button(root, text="9", command=numero9, width=8, height=2).grid(row=4, column=2)
+Num0 = Button(root, text="0", command=numero0, width=8, height=2).grid(row=5, column=0)
+
+Somar = Button(root, text="+", command = somar, width=8, height=2).grid(row=6, column=0)
+Dividir = Button(root, text="/", command = dividir, width=8, height=2).grid(row=7, column=0)
+Multiplicar = Button(root, text="*", command = multiplicar, width=8, height=2).grid(row=7, column=1)
+Subtrair = Button(root, text="-", command = subtrair, width=8, height=2).grid(row=7, column=2)
+
+Limpar = Button(root, text="Limpar", command=limpar, width=18, height=2).grid(row=5, column=1, columnspan=2)
+Resultado = Button(root, text="=", command = result, width=18, height=2).grid(row=6, column=1, columnspan=2)
 
 root.mainloop()
